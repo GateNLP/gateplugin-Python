61df8f4d77092a9cea232ee017b8221d9d5746bb
Dominic Rout <dom.rout@gmail.com>
2017-01-27 16:16:42 +0000

    Add a setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name="gate-python",
+    version="0.1",
+    packages=find_packages(),
+
+    author="Dominic Rout",
+    author_email="d.rout@sheffield.ac.uk", 
+    license="lgpl"
+)
