6f3eb00416a5433380eb6b5092bf9d7882fef157
David Jones <drj@pobox.com>
2019-08-09 13:40:38 +0100

    Add series of parameterised tests

--- gate/test_sourcedstring.py
+++ gate/test_sourcedstring.py
@@ -39,8 +39,26 @@ class TestSomething(unittest.TestCase):
         bs = begins(l)
         assert bs == [0, 2, 4]
 
+
 def begins(seq):
     """
     Collect all the .begin values for a sequence of sourced strings.
     """
     return [ss.source.begin for ss in seq]
+
+
+# A series of parameterised tests
+def instantiate_test_split(n):
+    def test_split_n(self):
+        s = sourcedstring.SimpleSourcedUnicodeString("banana", None)
+        l = s.split("n", n)
+        print(l)
+        bs = begins(l)
+        assert bs == [0, 3, 5][:n+1]
+    return test_split_n
+
+
+for n in range(4):
+    name = "test_split_n_{}".format(n)
+    test_fun = instantiate_test_split(n)
+    setattr(TestSomething, name, test_fun)
