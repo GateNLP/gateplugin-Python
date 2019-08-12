b4e43c90887106f9dd0f91ee03daa0b6a3b324d6
David Jones <drj@pobox.com>
2019-08-12 11:12:18 +0100

    Remove noisy prints from tests

--- gate/test_sourcedstring.py
+++ gate/test_sourcedstring.py
@@ -14,35 +14,30 @@ class TestSomething(unittest.TestCase):
     def test_split(self):
         s = sourcedstring.SimpleSourcedUnicodeString("banana", None)
         l = s.split("n")
-        print(l)
         bs = begins(l)
         assert bs == [0, 3, 5]
 
     def test_split_default(self):
         s = sourcedstring.SimpleSourcedUnicodeString("a ban na", None)
         l = s.split()
-        print(l)
         bs = begins(l)
         assert bs == [0, 2, 6]
 
     def test_rsplit(self):
         s = sourcedstring.SimpleSourcedUnicodeString("banana", None)
         l = s.rsplit("n")
-        print(l)
         bs = begins(l)
         assert bs == [0, 3, 5]
 
     def test_rsplit_0(self):
         s = sourcedstring.SimpleSourcedUnicodeString("banana", None)
         l = s.rsplit("n", 0)
-        print(l)
         bs = begins(l)
         self.assertEqual(bs, [0])
 
     def test_rsplit_default(self):
         s = sourcedstring.SimpleSourcedUnicodeString("b n na", None)
         l = s.rsplit()
-        print(l)
         bs = begins(l)
         assert bs == [0, 2, 4]
 
@@ -61,7 +56,6 @@ def instantiate_test_split(n):
     def test_split_n(self):
         s = sourcedstring.SimpleSourcedUnicodeString("banana", None)
         l = s.split("n", n)
-        print(l)
         bs = begins(l)
         self.assertEqual(bs, [0] + [3, 5][:n])
     return test_split_n
@@ -71,7 +65,6 @@ def instantiate_test_rsplit(n):
     def test_rsplit_n(self):
         s = sourcedstring.SimpleSourcedUnicodeString("banana", None)
         l = s.rsplit("n", n)
-        print(l)
         bs = begins(l)
         self.assertEqual(bs, [0] + [3, 5][-n:])
     return test_rsplit_n
