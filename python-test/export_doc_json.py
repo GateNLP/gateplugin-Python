536d3b646de5de3e0ddd7eb0ad0b31845160c8a0
Johann Petrak <johann.petrak@gmail.com>
2019-11-24 14:47:41 +0000

    Clean up. Only keep what we need for version 2.0, we can still add back converted stuff like groovy scripts later.

--- python-test/export_doc_json.py
+++
@@ -1,9 +0,0 @@
-import gate, sys
-
-if __name__ == '__main__':
-	gate = gate.Gate()
-	gate.start()
-	result = gate.loadAsJSON(sys.argv[1])
-	gate.stop()
-	with open(sys.argv[2], "w") as f:
-		print >> f, result
