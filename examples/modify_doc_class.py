4a888390b4469d06eb262d69dd96429ea5509f8e
Johann Petrak <johann.petrak@gmail.com>
2019-11-25 15:05:44 +0000

    Add test for printing to stderr and stdout

--- examples/modify_doc_class.py
+++ examples/modify_doc_class.py
@@ -11,6 +11,8 @@ class MyProcessor:
   def finish(**kwargs):
     print("Running finish(), kwargs={}".format(kwargs), file=sys.stderr)
   def __call__(doc, **kwargs):
+    print("SOMETHING FOR STDOUT")
+    print("SOMETHING FOR STDERR", file=sys.stderr)
     print("We are running on a doc! kwargs={}".format(kwargs), file=sys.stderr)
     set1 = doc.get_annotations("Set1")
     set1.clear()
