0dc9f4611c9ae590da67959ebd4d3ccd0b2e9d7b
Johann Petrak <johann.petrak@gmail.com>
2019-12-06 11:02:11 +0100

    Improved example

--- examples/tokenize_doc_class.py
+++ examples/tokenize_doc_class.py
@@ -8,23 +8,27 @@ from gatenlp import interact, GateNlpPr, Document
 class MyProcessor:
 
   def __init__(self):
-    print("DEBUG: running __init__")
     self.tokens_total = 0
+    self.nr_docs = 0
   def start(self, **kwargs):
-    print("DEBUG: running start")
     self.tokens_total = 0
+    self.nr_docs = 0
   def finish(self, **kwargs):
-    print("DEBUG: running finish")
+    print("Total number of tokens:", self.tokens_total)
+    print("Number of documents:", self.nr_docs)
   def __call__(self, doc, **kwargs):
     set1 = doc.get_annotations()  
     set1.clear()   
     text = doc.text  
-    whitespaces = [m for m in re.finditer(r"[\s,.!?]+|^[\s,.!?]*|[\s,.!?]*$",text)] 
-    for k in range(len(whitespaces)-1):  
+    whitespaces = [m for m in re.finditer(r"[\s,.!?]+|^[\s,.!?]*|[\s,.!?]*$",text)]
+    nrtokens = len(whitespaces)-1
+    for k in range(nrtokens):  
         fromoff=whitespaces[k].end()   
         tooff=whitespaces[k+1].start() 
         set1.add(fromoff, tooff, "Token", {"tokennr": k})
-    doc.set_feature("nr_tokens", len(whitespaces)-1)
+    doc.set_feature("nr_tokens", nrtokens)
+    self.tokens_total += nrtokens
+    self.nr_docs += 1
 
     
 if __name__ == '__main__':
