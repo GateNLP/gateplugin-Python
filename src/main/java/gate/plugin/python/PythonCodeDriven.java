e6cb2b3835734dc228cc465a64967cf863f73419
Johann Petrak <johann.petrak@gmail.com>
2019-12-09 20:41:24 +0100

    Add license headers

--- src/main/java/gate/plugin/python/PythonCodeDriven.java
+++ src/main/java/gate/plugin/python/PythonCodeDriven.java
@@ -1,21 +1,21 @@
-/* 
- * Copyright (C) 2014-2016 The University of Sheffield.
+/*
+ * Copyright (c) 2019 The University of Sheffield.
  *
- * This file is part of gateplugin-Java
- * (see https://github.com/johann-petrak/gateplugin-Java)
+ * This file is part of gateplugin-python 
+ * (see https://github.com/GateNLP/gateplugin-python).
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package gate.plugin.python;
 
--- src/main/java/gate/plugin/python/PythonPr.java
+++ src/main/java/gate/plugin/python/PythonPr.java
@@ -1,20 +1,21 @@
-/* 
- * Copyright (C) 2014-2016 The University of Sheffield.
+/*
+ * Copyright (c) 2019 The University of Sheffield.
  *
- * This file is part of gateplugin-python
+ * This file is part of gateplugin-python 
+ * (see https://github.com/GateNLP/gateplugin-python).
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package gate.plugin.python;
