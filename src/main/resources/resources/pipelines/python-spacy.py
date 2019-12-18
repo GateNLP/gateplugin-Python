1dcc7d1827a917336bf815f1844a44d185cfc165
Johann Petrak <johann.petrak@gmail.com>
2019-12-18 22:25:59 +0100

    Updated spacy PR. Turns out spacy involves space tokens and new line tokens in the edges of the dependency parser, how odd is that?

--- src/main/resources/resources/pipelines/python-spacy.py
+++ src/main/resources/resources/pipelines/python-spacy.py
@@ -23,6 +23,7 @@ class MyProcessor:
     print("Total number of tokens:", self.tokens_total)
     print("Number of documents:", self.nr_docs)
   def __call__(self, doc, **kwargs):
+    print("PROCESSING: ",doc.get_feature("gate.plugin.python.docName"))
     if "outputAnnotationSet" in kwargs:
         set1 =  doc.get_annotations(kwargs.get("outputAnnotationSet"))
     else:
@@ -30,61 +31,64 @@ class MyProcessor:
     set1.clear()   
     text = doc.text  
     doc = self.nlp(text)
-    tokidx2annid = {}
+    toki2annid = {}
     for tok in doc:
         from_off = tok.idx
         to_off = tok.idx + len(tok)
         is_space = tok.is_space
-        if not is_space:
-            fm = {
-                    "is_alpha": tok.is_alpha,
-                    "is_bracket": tok.is_bracket,
-                    "is_currency": tok.is_currency,
-                    "is_digit": tok.is_digit,
-                    "is_left_punct": tok.is_left_punct,
-                    "is_lower": tok.is_lower,
-                    "is_oov": tok.is_oov,
-                    "is_punct": tok.is_punct,
-                    "is_quote": tok.is_quote,
-                    "is_right_punct": tok.is_right_punct,
-                    "is_sent_start": tok.is_sent_start,
-                    "is_space": tok.is_space,
-                    "is_stop": tok.is_stop,
-                    "is_title": tok.is_title,
-                    "is_upper": tok.is_upper,
-                    "lang": tok.lang_,
-                    "lemma": tok.lemma_,
-                    "like_email": tok.like_email,
-                    "like_num": tok.like_num,
-                    "like_url": tok.like_url,
-                    "orth": tok.orth,
-                    "pos": tok.pos_,
-                    "prefix": tok.prefix_,
-                    "prob": tok.prob,
-                    "rank": tok.rank,
-                    "sentiment": tok.sentiment,
-                    "tag": tok.tag_,
-                    "shape": tok.shape_,
-                    "suffix": tok.suffix_,
-            }
-            if doc.is_nered:
-                fm["ent_type"] = tok.ent_type_
-            if doc.is_parsed:
-                fm["dep"] = tok.dep_
-            annid = set1.add(from_off, to_off, "Token", fm)
-            print("Added annotation with id: {}".format(annid))
-            tokidx2annid[tok.idx] = annid
-            ws = tok.whitespace_
-            # TODO: if ws is not length 0, could add space token here
+        fm = {
+                "_i": tok.i,
+                "is_alpha": tok.is_alpha,
+                "is_bracket": tok.is_bracket,
+                "is_currency": tok.is_currency,
+                "is_digit": tok.is_digit,
+                "is_left_punct": tok.is_left_punct,
+                "is_lower": tok.is_lower,
+                "is_oov": tok.is_oov,
+                "is_punct": tok.is_punct,
+                "is_quote": tok.is_quote,
+                "is_right_punct": tok.is_right_punct,
+                "is_sent_start": tok.is_sent_start,
+                "is_space": tok.is_space,
+                "is_stop": tok.is_stop,
+                "is_title": tok.is_title,
+                "is_upper": tok.is_upper,
+                "lang": tok.lang_,
+                "lemma": tok.lemma_,
+                "like_email": tok.like_email,
+                "like_num": tok.like_num,
+                "like_url": tok.like_url,
+                "orth": tok.orth,
+                "pos": tok.pos_,
+                "prefix": tok.prefix_,
+                "prob": tok.prob,
+                "rank": tok.rank,
+                "sentiment": tok.sentiment,
+                "tag": tok.tag_,
+                "shape": tok.shape_,
+                "suffix": tok.suffix_,
+        }
+        if doc.is_nered:
+            fm["ent_type"] = tok.ent_type_
+        if doc.is_parsed:
+            fm["dep"] = tok.dep_
+        if tok.is_space:
+        	anntype = "SpaceToken"
         else:
-            pass # could add space token here
+        	anntype = "Token"
+        annid = set1.add(from_off, to_off, anntype, fm)
+        toki2annid[tok.i] = annid
+        # print("Added annotation with id: {} for token {}".format(annid, tok.i))
+        ws = tok.whitespace_
+        if len(ws) > 0:
+        	set1.add(to_off, to_off+len(ws), "SpaceAddedToken", {"is_space": True})
     # if we have a dependency parse, now also add the parse edges
     if doc.is_parsed:
         for tok in doc:
-            ann = set1.get(tokidx2annid[tok.idx])
-            ann.set_feature("head", tokidx2annid[tok.head.idx])
-            ann.set_feature("left_edge", tokidx2annid[tok.left_edge.idx])
-            ann.set_feature("right_edge", tokidx2annid[tok.right_edge.idx])
+            ann = set1.get(toki2annid[tok.i])
+            ann.set_feature("head", toki2annid[tok.head.i])
+            ann.set_feature("left_edge", toki2annid[tok.left_edge.i])
+            ann.set_feature("right_edge", toki2annid[tok.right_edge.i])
     if doc.ents:
         for ent in doc.ents:
             set1.add(ent.start_char, ent.end_char, ent.label_, {"lemma": ent.lemma_})
