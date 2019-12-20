f89347b48e5bae0acd94d1902775dce64c2f5369
Johann Petrak <johann.petrak@gmail.com>
2019-12-20 14:08:59 +0000

    Some more examples for debugging.

---
+++ examples/debug1.py
@@ -0,0 +1,18 @@
+
+import sys
+from gatenlp import interact, GateNlpPr, Document
+
+@GateNlpPr
+class MyProcessor:
+
+  def __call__(self, doc, **kwargs):
+    set2 = doc.get_annotations("Copy")
+    set2.clear()
+    set_def = doc.get_annotations()
+    for ann in set_def:
+        annid = set2.add(ann.start, ann.end, ann.type, ann.features)
+        ann = set2.get(annid)
+        ann.set_feature("python_start", ann.start)
+        ann.set_feature("python_end", ann.end)
+
+interact()
