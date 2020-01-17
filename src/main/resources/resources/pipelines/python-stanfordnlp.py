ee8ffdd62f7f4622d5f74e1b3f777c4c262c2c02
Johann Petrak <johann.petrak@gmail.com>
2020-01-17 16:08:03 +0100

    Add initial ready made pipeline for stanfordnlp.

--- src/main/resources/resources/pipelines/python-stanfordnlp.py
+++ src/main/resources/resources/pipelines/python-stanfordnlp.py
@@ -1,7 +1,8 @@
 """Run spacy"""
 
 from gatenlp import interact, GateNlpPr, Document
-from gatenlp.spacy import apply_spacy
+import stanfordnlp
+from gatenlp.stanfordnlp import apply_stanfordnlp
 import sys
 import re
 import spacy
@@ -14,23 +15,19 @@ class MyProcessor:
     self.tokens_total = 0
     self.nr_docs = 0
   def start(self, **kwargs):
-    if "spacyModel" in kwargs:
-        self.nlp = spacy.load(kwargs.get("spacyModel"))
-    else:
-        self.nlp = spacy.load("en_core_web_sm")
+    self.nlp = stanfordnlp.Pipeline()
     self.tokens_total = 0
     self.nr_docs = 0
   def finish(self, **kwargs):
     print("Total number of tokens:", self.tokens_total)
     print("Number of documents:", self.nr_docs)
   def __call__(self, doc, **kwargs):
-    # print("PROCESSING: ",doc.get_feature("gate.plugin.python.docName"))
     outset = ""
     if "outputAnnotationSet" in kwargs:
         outset =  kwargs.get("outputAnnotationSet")
     annset = doc.get_annotations(outset)
     annset.clear()
-    apply_spacy(self.nlp, doc, setname=outset) 
+    apply_stanfordnlp(self.nlp, doc, setname=outset) 
     
     self.tokens_total += len(doc)    
     self.nr_docs += 1
