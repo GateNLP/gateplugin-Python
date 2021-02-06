bfd169a5836b859ce01e76272fbb5f979b65fa1a
Johann Petrak <johann.petrak@gmail.com>
2021-02-06 19:05:39 +0100

    Prepare version 2.4

--- examples/startPythonSlave.groovy
+++ examples/startPythonSlave.groovy
@@ -3,7 +3,7 @@ import gate.creole.*
 
 Gate.init();
 // TODO: try to load from the properties file that we create ... ?
-Gate.getCreoleRegister().registerPlugin(new Plugin.Maven("uk.ac.gate.plugins", "python", "2.0-SNAPSHOT"));
+Gate.getCreoleRegister().registerPlugin(new Plugin.Maven("uk.ac.gate.plugins", "python", "2.4"));
 
 parms = Factory.newFeatureMap();
 lr = Factory.createResource("gate.plugin.python.PythonSlaveLr", parms)
