8e9f3ec8b74774ab3ec10ae43983390f7d8fa449
Johann Petrak <johann.petrak@gmail.com>
2021-02-25 13:09:34 +0100

    Add progress messages.

--- examples/count_words_reduce.py
+++ examples/count_words_reduce.py
@@ -10,11 +10,14 @@ class MyProcessor:
     self.words_total = 0
     self.nr_docs = 0
   def start(self, **kwargs):
+    print("Starting MyProcessor, kwargs=", kwargs)
     self.words_total = 0
     self.nr_docs = 0
   def finish(self, **kwargs):
+    print("Finishing MyProcessor, kwargs=", kwargs)
     return {"nrdocs": self.nr_docs, "nrwords": self.words_total}
   def reduce(self, resultlist):
+    print("Running MyProcessor.reduce with result=", resultlist)
     totals = Counter()
     for r in resultlist:
       totals.update(r)
